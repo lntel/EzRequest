55e99ecaf47ffdca0b2e00c753c0cf2db82a3e60
Casper <joeharris461767@gmail.com>
2019-06-21 13:42:35 +0100

    Updated

--- EzRequest/EzRequest.cpp
+++ EzRequest/EzRequest.cpp
@@ -1,7 +1,23 @@
 #include "pch.h"
 #include <iostream>
+#include "Request.h"
+#include "Json.h"
+
+using namespace EzRequest;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	Json json;
+	Request req;
+
+	string data = json.serialize(map<string, string>({
+		{
+			"username", "test"
+		},
+		{
+			"password", "testing"
+		}
+	}));
+
+	req.postBody("http://localhost/v2/user/login.php", data);
 }
