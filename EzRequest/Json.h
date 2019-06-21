c9f7e44692caae8e51460b5c6994715f7510126d
Casper <joeharris461767@gmail.com>
2019-06-21 13:42:12 +0100

    Adding Json class

---
+++ EzRequest/Json.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "pch.h"
+
+using namespace std;
+
+namespace EzRequest {
+
+	class Json {
+	public:
+
+		string serialize(map<string, string> data)
+		{
+			string _d;
+
+			_d = "{";
+
+			map<string, string>::iterator it;
+
+			for (it = data.begin(); it != data.end(); ++it)
+			{
+				if (it == prev(data.end()))
+				{
+					_d += "\"" + it->first + "\": \"" + it->second + "\"}";
+				}
+
+				_d += "\"" + it->first + "\": \"" + it->second + "\", ";
+			}
+
+			return _d;
+		}
+
+	};
+
+}
